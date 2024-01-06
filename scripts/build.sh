de2e71578e9de6669eabd84455b5d19586ba63f7
Eric Bittleman <eric.bittleman@gmail.com>
2024-01-06 09:01:36 -0500

    config: moved TAB to upper z

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,16 @@
 set -x
 
 pushd ../zmk
-source .venv/bin/activate
+
+if [ -f .venv/bin/activate ]; then
+    source .venv/bin/activate
+elif [ -f .venv/Scripts/activate ]; then
+    source .venv/Scripts/activate
+else
+    echo "Setup a Python virtual environment for ZMK please"
+    exit 1
+fi
+
 cd app
 west build -d build/left
 west build -d build/right
